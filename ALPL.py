d2c641542d309d9930d1756f26ebfc56a7dd9a00
locha0305 <69513450+locha0305@users.noreply.github.com>
2021-01-16 00:06:41 +0900

    Updated Class creation

--- ALPL.py
+++ ALPL.py
@@ -7,7 +7,11 @@ class runtime():
         self.code = code
         self.parser = Parser.parser(self.code) #get parse result
         self.code = self.parser.parse()
-        self.globals = {"CLASS" : Classes.Class, "FUNCTION" : Classes.Function}
+        #Class
+        self.main = Classes.Class()
+        #Class object
+        self.main.add_attr("CLASS", Classes.Class)
+        self.main.attributes["CLASS"].__init__(self.main.attributes["CLASS"]) #initiallize the Class class
     def execute(self): #this executes the code
         cursor = 0
         while cursor < len(self.code):
@@ -19,23 +23,21 @@ class runtime():
                     definition.append(self.code[cursor + jump])
                     jump += 1
                 cursor += jump
-                self.mother = definition[-1] #the last thing is the name of the created Class
-                self.globals[self.mother] = self.globals["CLASS"]() #create a Class
-                print(self.globals)
-            elif self.current_TT == "FUNC": #handles the functions
-                jump = 2
-                definition = []
-                while self.code[cursor + jump] != "END": #check for definitions
-                    definition.append(self.code[cursor + jump])
-                    jump += 1
-                cursor += jump
-                self.mother = definition[-1] #the last thing is the name of the created Function
-                self.globals[self.mother] = self.globals["FUNCTION"]() #create a Function
-                print(self.globals)
-            
+                self.daughter = definition[-1] #the last thing is the name of the created Class
+                self.mother = definition[0]
+                if len(definition) == 2: #when a top class exists
+                    pass
+                else:
+                    self.mother = "CLASS"
+                self.main.add_attr(self.daughter, self.main.attributes["CLASS"]())
+                self.main.attributes[self.daughter].attributes = self.main.attributes[self.mother].attributes #get the characteristics of it's mother class
+                self.main.attributes["b"].attributes = {"Y" : 1, "X" : 12 ** 3}
+                print(self.main.attributes)
             cursor += 1
-    
+
         
-Rt = runtime("Class a b c d{\nFunc f{\n}\n}")
+Rt = runtime("Class b{}\nClass a{}")
 Rt.execute()
+print(Rt.main.attributes["a"].attributes)
+
 
